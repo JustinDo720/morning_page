284850d2f405ffd1288a11fcf6b43bc858d4b8ae
Justin <justindo720@gmail.com>
2020-10-30 23:25:16 -0400

    Changed layout to divs being in one whole container for each topic.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,74 +4,104 @@
       <h1>
         {{ r1l_title }}
       </h1>
+      <div class="quote-sub">
+        <h4>
+          “You have a viable business only if your product is either better or cheaper than the alternatives.
+          If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+        </h4>
+        <br>
+        <h6>
+          -"Jim Koch, founder of Boston Beer Co."
+        </h6>
+      </div>
     </div>
-    <div id="app-todo">
+    <div id="app-todo" class="todo">
       <h1>
         {{ r1m_title }}
       </h1>
+      <div class="todo-sub">
+        <h4>
+          “You have a viable business only if your product is either better or cheaper than the alternatives.
+          If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+        </h4><br>
+        <h6>
+          -"Jim Koch, founder of Boston Beer Co."
+        </h6><h4>
+        “You have a viable business only if your product is either better or cheaper than the alternatives.
+        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+      </h4>
+        <br>
+        <h6>
+          -"Jim Koch, founder of Boston Beer Co."
+        </h6><h4>
+        “You have a viable business only if your product is either better or cheaper than the alternatives.
+        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+      </h4>
+        <h4>
+          “You have a viable business only if your product is either better or cheaper than the alternatives.
+          If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+        </h4>
+        <h4>
+          “You have a viable business only if your product is either better or cheaper than the alternatives.
+          If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+        </h4>
+        <h4>
+          “You have a viable business only if your product is either better or cheaper than the alternatives.
+          If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+        </h4>
+        <h4>
+          “You have a viable business only if your product is either better or cheaper than the alternatives.
+          If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+        </h4><h4>
+        “You have a viable business only if your product is either better or cheaper than the alternatives.
+        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+      </h4><h4>
+        “You have a viable business only if your product is either better or cheaper than the alternatives.
+        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+      </h4>
+      </div>
     </div>
     <div id="app-news">
       <h1>
         {{ r1r_title }}
       </h1>
-    </div>
-    <div class="quote">
-      <h4>
-        “You have a viable business only if your product is either better or cheaper than the alternatives.
-        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
-      </h4>
-      <br>
-      <h6>
-        -"Jim Koch, founder of Boston Beer Co."
-      </h6>
-    </div>
-    <div class="todo">
-      <h4>
-        “You have a viable business only if your product is either better or cheaper than the alternatives.
-        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
-      </h4>
-      <br>
-      <h6>
-        -"Jim Koch, founder of Boston Beer Co."
-      </h6><h4>
-      “You have a viable business only if your product is either better or cheaper than the alternatives.
-      If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
-    </h4>
-      <br>
-      <h6>
-        -"Jim Koch, founder of Boston Beer Co."
-      </h6><h4>
-      “You have a viable business only if your product is either better or cheaper than the alternatives.
-      If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
-    </h4>
-      <br>
-      <h6>
-        -"Jim Koch, founder of Boston Beer Co."
-      </h6><h4>
-      “You have a viable business only if your product is either better or cheaper than the alternatives.
-      If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
-    </h4>
-      <br>
-      <h6>
-        -"Jim Koch, founder of Boston Beer Co."
-      </h6><h4>
-      “You have a viable business only if your product is either better or cheaper than the alternatives.
-      If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
-    </h4>
-      <br>
-      <h6>
-        -"Jim Koch, founder of Boston Beer Co."
-      </h6>
-    </div>
-    <div class="news">
+      <div class="news-1">
+        <h1>
+          HelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+        </h1>
+        <h1>
+          HelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+        </h1><h1>
+        HelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+      </h1>
+        <h1>
+          HelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+        </h1>
 
+
+      </div>
     </div>
+
     <div class="weather">
       <h1>
         {{ r2l_title }}
       </h1>
-
+      <div class="weather-sub">
+        <h6>
+          -"Jim Koch, founder of Boston Beer Co."
+        </h6><h4>
+        “You have a viable business only if your product is either better or cheaper than the alternatives.
+        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+      </h4>
+        <h6>
+          -"Jim Koch, founder of Boston Beer Co."
+        </h6><h4>
+        “You have a viable business only if your product is either better or cheaper than the alternatives.
+        If it’s not one or the other, you might make some money at first, but it’s not a sustainable business."
+      </h4>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -92,6 +122,7 @@ export default{
 </script>
 
 <style scoped>
+/* Grid Container */
 .grid-container{
   display:grid;
   grid-template-columns: 1fr 2fr 1fr;
@@ -110,10 +141,19 @@ export default{
   text-align:center
 }
 
+/* To-Do List */
 .todo{
   grid-column-start: 2;
-  grid-row-start: 2;
+  grid-row-start: 1;
   grid-row-end: 5;
 }
 
+/* Weather */
+
+.weather{
+  align-items: start;
+}
+
+/* News */
+/* Quote of the Day */
 </style>
