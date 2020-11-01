ef258503e065b9700e18be601c64b664bfd03936
Justin <justindo720@gmail.com>
2020-10-31 21:46:00 -0400

    Finished with a basic todo which has a button to submit their inputs and pressing the enter key

--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -2,6 +2,11 @@
   <h1>
     {{title}}
   </h1>
+  <h4 v-for="(todo, key) in todo_list" :key="key">
+    {{todo.todo_item}}
+  </h4>
+  <input placeholder="Add Item" v-model="todo" @keyup.enter="add_todo"> &nbsp;
+  <button type="submit" @click="add_todo">Add to your To-Do List</button>
 </template>
 <script>
 
@@ -9,7 +14,21 @@ export default{
   name: 'todo',
   data(){
     return{
-      title: 'To-Do-List'
+      title: 'To-Do-List',
+      todo: '',
+      todo_list: [],
+      task_completed: false,
+    }
+  },
+  methods:{
+    add_todo: function(){
+      // We are going to create an object and set some keys to values in which will be pushed to the list
+      let todo_info = {}
+      todo_info['todo_item'] = this.todo
+      todo_info['completed'] = this.task_completed
+      this.todo_list.push(todo_info)
+      // Resets the input filed to become blank
+      this.todo = ''
     }
   }
 }
