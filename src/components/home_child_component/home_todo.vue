c2aa05fe2fc0361593cce9835c8b82e49a96c50c
Justin <justindo720@gmail.com>
2020-11-12 21:40:55 -0500

    Added a search box for the todo list in the home page.

--- src/components/home_child_component/home_todo.vue
+++ src/components/home_child_component/home_todo.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-for="(todo,index) in fetched_todos" :key="index">
+  <div id="search_box">
+    <input class='searchBox' placeholder="Search For Your Todos" v-model="searchTodo">
+  </div>
+  <div v-for="(todo,index) in filteredTodos" :key="index">
     <h1 id="todo_item">
       {{todo.todo_item}}
     </h1>
@@ -16,6 +19,7 @@ export default{
   data(){
     return{
       title: 'Home Todo',
+      searchTodo: '',
       fetched_todos: [], // In progress todos or the ones that are neutral
       firebase_db_url: 'https://morningpage-aa0e4.firebaseio.com/post.json'
     }
@@ -32,6 +36,13 @@ export default{
       }
       this.fetched_todos = firebase_todos
     })
+  },
+  computed:{
+    filteredTodos: function(){
+      return this.fetched_todos.filter(todo=>{
+        return todo.todo_item.match(this.searchTodo)
+      })
+    }
   }
 }
 </script>
@@ -41,5 +52,20 @@ export default{
 }
 #task_date{
   text-align: left;
+}
+.searchBox{
+  width: 600px;
+  height: 40px;
+  border: 1px solid;
+  border-radius: 3px;
+  font-size: 15px;
+  padding: 0 20px;
+  line-height: 30px;
+  -moz-transition: all 0.4s ease; /* Transition for Mozilla! */
+  -webkit-transition: all 0.4s ease; /* For other browsers */
+}
+.searchBox:focus{
+  box-shadow: 0 1px 5px 0, rgba(0,0,0,0.15);
+  transform: translateY(-3px);
 }
 </style>
