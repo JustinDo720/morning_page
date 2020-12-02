09529ece0518d717ff86ef92070db90b5ea40b6a
I am you <justindo720@gmail.com>
2020-12-02 11:00:06 -0500

    Connected App with OpenWeatherMap api. Currently the weather is set to London as default but we could change that later on with maybe a user pereferencepereference

--- src/components/home_child_component/home_weather.vue
+++ src/components/home_child_component/home_weather.vue
@@ -4,12 +4,38 @@
   </h1>
 </template>
 <script>
+import axios from 'axios'
 
 export default{
   data(){
     return{
       title: 'Home Weather'
     }
+  },
+  created(){
+    // Our params are dummy data we could patch it up later
+    const options = {
+      method: 'GET',
+      url: process.env.VUE_APP_WEATHER_URL,
+      params: {
+        q: 'London,uk',
+        lat: '0',
+        lon: '0',
+        callback: 'test',
+        id: '2172797',
+        lang: 'null',
+        units: '"metric" or "imperial"',
+        mode: 'xml, html'
+      },
+      headers: {
+        'x-rapidapi-key': process.env.VUE_APP_API_KEY,
+        'x-rapidapi-host': process.env.VUE_APP_WEATHER_HOST
+      }
+    }
+
+    axios.request(options).then((res) =>{
+      console.log(res.data)
+    })
   }
 }
 </script>
