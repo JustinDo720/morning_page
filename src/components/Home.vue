f36bead7fb9d09fae579eaa24d934e2d36ad6f41
I am you <justindo720@gmail.com>
2021-01-25 22:10:30 -0500

    Updated News style. Every link as a border.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -28,199 +28,12 @@
       </div>
     </div>
 
-    <div class="news card">
+    <div class="news card light-blue lighten-2">
       <div class="card-content">
         <h4>
           {{ r1r_title }}
         </h4>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/2b0a93c2-d190-336d-a5b7-54f8d7b97500/bold-prediction%3A-general.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><p class="flow-text indigo-text darken-3">
-              Bold Prediction: General Motors Is the Next Apple Stock for
-              Investors
-            </p></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/news/benzingas-final-bulls-bears-alibaba-162530018.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Benzinga's Final Bulls And Bears Of The Year: Alibaba, Apple,
-              Intel, Tesla And More
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/b9cdc2cf-a61e-36b0-b764-166c5e094109/why-apple-rose-12.3%25-in.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">Why Apple Rose 12.3% in December</h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/news/exploding-debt-levels-mean-stocks-221234260.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Exploding Debt Levels Mean Stocks Can't Go ‘To the
-              Moon’&nbsp;Forever
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/daa6e775-09c1-3e0b-83e0-5ddb6ab56433/apple-stock%3A-buy%2C-sell%2C-or.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Apple Stock: Buy, Sell, or Hold After Soaring 81% in 2020?
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/55936da7-0b9c-39f5-b471-0c6551a229bc/square-and-paypal-shouldn%27t.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Square and PayPal Shouldn't Fear Google's Fintech Push
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/news/best-worst-dow-stocks-2020-215552073.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              The Best and Worst Dow Stocks From 2020
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/72049339-a3bb-3224-986d-d8556854ab12/there%E2%80%99s-reason-to-be-hopeful.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              There’s Reason to Be Hopeful About Intel Stock in 2021. Here’s
-              Why.
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/c33b62ff-8e23-30df-8ce9-1695ec1c806c/apple-leads-dow-jones-stocks.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Apple Leads Dow Jones Stocks In 2020 But Q4 Winner Dominates
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/65b53896-faf4-3a06-9d0d-a63cf3c83192/dow-jones-stocks-to-buy-and.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Dow Jones Stocks To Buy And Watch In December 2020: Apple Eyes New
-              Buy Point
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/news/4-top-stock-trades-monday-205221463.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              4 Top Stock Trades for Monday: AAPL, GLD, MRNA, CMI
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/9eb13eb0-24b0-3deb-9013-a0651d2a2abb/these-7-value-stocks-deserve.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              These 7 Value Stocks Deserve a Fresh Look in 2021
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/video/see-value-dogs-dow-kendall-201405048.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              We see value in the ‘dogs of the DOW’: Kendall Capital CEO
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/8a9bb70b-954f-3971-9448-b737298a8ac3/dow-jones-gains-as-schumer.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Dow Jones Gains As Schumer Offers McConnell Stimulus Bargain;
-              Tesla Surges, Apple Falls Again
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/d8629cd3-0a89-3a0e-9091-bed4e56c9f69/big-tech-has-an-antitrust.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Big Tech has an antitrust target on its back, and it is only going
-              to get bigger
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/80a2d720-9b22-30ea-bc05-874d5dcf4f16/what-mark-zuckerberg-and.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              What Mark Zuckerberg and Cornelius Vanderbilt Have In Common
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/video/movie-industry-strengthen-again-around-193555079.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              The movie industry will strengthen again around ‘April or May’:
-              Screenvision CEO
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/87f6a358-58a2-318c-a17b-a7a514da8c6e/how-to-invest-in-stocks%3A.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              How To Invest In Stocks: Stock Market Investing For Beginners In
-              2021
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/8f9abdd4-8a67-3548-bddb-8285c92d9dba/dow-jones-flat-as-stock.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Dow Jones Flat As Stock Market Set To Close 2020 Near Record Highs
-            </h3></a
-          >
-        </div>
-        <div data-v-3077afae="">
-          <a
-            href="https://finance.yahoo.com/m/ba63866c-722e-3876-ae16-a27b122f6dc8/apple-iphone-12-pro-demand.html?.tsrc=rss"
-            data-v-3077afae=""
-            ><h3 data-v-3077afae="">
-              Apple iPhone 12 Pro Demand Strong, Says J.P. Morgan
-            </h3></a
-          >
-        </div>
-        <!--<home_news></home_news>-->
+        <home_news></home_news>
       </div>
     </div>
 
@@ -237,7 +50,7 @@
 
 <script>
 import home_todo from "@/components/home_child_component/home_todo";
-//import home_news from "@/components/home_child_component/home_news";
+import home_news from "@/components/home_child_component/home_news";
 //import home_quote_of_day from "@/components/home_child_component/home_quote_of_day";
 import home_weather from "@/components/home_child_component/home_weather";
 //import Modal from '@/components/utilities/Modal.vue';
@@ -250,7 +63,7 @@ export default {
     home_todo,
     //modal_test: Modal,
     //home_quote_of_day,
-    //home_news,
+    home_news,
     home_weather
   },
   data() {
--- src/components/home_child_component/home_news.vue
+++ src/components/home_child_component/home_news.vue
@@ -1,10 +1,12 @@
 <template>
-  <div v-for="(news, news_id) in news_from_request" :key="news_id">
-    <a :href="news.link">
-      <p class="flow-text indigo-text darken-3">
-        {{ news.title }}
-      </p>
-    </a>
+  <div class='newsContainer'>
+    <div v-for="(news, news_id) in news_from_request" :key="news_id" class='newsBox light-blue lighten-3'>
+      <a :href="news.link">
+        <p class="flow-text indigo-text darken-3">
+          {{ news.title }}
+        </p>
+      </a>
+    </div>
   </div>
 </template>
 <script>
@@ -63,5 +65,16 @@ a {
 
 h3 {
   color: black;
+}
+
+.newsBox{
+  border-radius: 5%;
+  margin:10px auto;
+  padding:10px;
+}
+
+.newsContainer{
+  overflow-y: scroll;
+  max-height: 700px;
 }
 </style>
